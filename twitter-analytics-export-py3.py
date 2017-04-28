148fe318cdf0ff0a4964121b56acfc308041b2ae
Mario Torres <mario291187@gmail.com>
2017-04-28 15:36:54 -0500

    Save output as SQLite db file

--- twitter-analytics-export-py3.py
+++ twitter-analytics-export-py3.py
@@ -9,9 +9,9 @@ import codecs
 import csv
 import argparse
 import os
+import sqlite3
 
-
-def twitter_flow(USERNAME, PASSWORD, ANALYTICS_ACCOUNT, NUM_DAYS, OUTPUT_DIRECTORY):
+def twitter_flow(USERNAME, PASSWORD, ANALYTICS_ACCOUNT, NUM_DAYS, OUTPUT_DIRECTORY, OUTPUT_TYPE):
     user_agent = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
     session = twitter_login(USERNAME, PASSWORD, user_agent)
     start_time, end_time = get_date_range(NUM_DAYS)
@@ -19,13 +19,24 @@ def twitter_flow(USERNAME, PASSWORD, ANALYTICS_ACCOUNT, NUM_DAYS, OUTPUT_DIRECTO
 
     split_data = format_data(data_string)
     outfile = get_filename(OUTPUT_DIRECTORY, start_time, end_time)
-    
-    with open(outfile, 'w',newline='') as f:
-        writer = UnicodeWriter(f)
-        for line in split_data:
-            writer.writerow(line)
 
-    print("CSV downloaded: ", outfile)
+    if args.t == 'sqlite':
+        writer = SQLiteWritter()
+        conn = sqlite3.connect( outfile )
+        writer.createtable(conn)
+        for line in split_data:
+            writer.writerow(line, conn)
+        conn.close()
+        print('Db file ready: ', outfile )
+    else:
+        # default: CSV
+        with open(outfile, 'w', newline='') as f:
+            writer = UnicodeWriter(f)
+
+            for line in split_data:
+                writer.writerow(line)
+
+            print("CSV downloaded: ", outfile)
 
 
 def twitter_login(user, pw, user_agent):
@@ -126,12 +137,10 @@ def format_data(data_string):
 
 def get_filename(output_dir, start_time, end_time):
     """Build descriptive filename for CSV"""
-    f_name = 'twitter_data_' + start_time + '_' + end_time + '.csv'
+    f_name = 'twitter_data_' + start_time + '_' + end_time + ( '.db' if args.t == 'sqlite' else '.csv')
     full_path = output_dir + '/' + f_name
 
     return full_path
-
-
 
 class UnicodeWriter: # grabbed from Python's csv module docs
     """
@@ -148,6 +157,7 @@ class UnicodeWriter: # grabbed from Python's csv module docs
 
     def writerow(self, row):
         self.writer.writerow([s for s in row])
+
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
 
@@ -162,7 +172,70 @@ class UnicodeWriter: # grabbed from Python's csv module docs
         for row in rows:
             self.writerow(row)
 
+class SQLiteWritter:
+    def createtable(self, conn):
+        c = conn.cursor()
 
+        # Drop table if exists
+        c.execute(''' drop table if EXISTS report ''')
+
+        # Create table
+        c.execute('''create table report(
+
+                                        tweet_id text,
+                                        tweet_permalink text,
+                                        tweet_text text,
+                                        time date,
+                                        impressions integer,
+                                        engagements integer,
+                                        engagement_rate numeric,
+                                        retweets integer,
+                                        replies integer,
+                                        likes integer,
+                                        user_profile_clicks integer,
+                                        url_clicks integer,
+                                        hashtag_clicks integer,
+                                        detail_expands integer,
+                                        permalink_clicks integer,
+                                        app_opens integer,
+                                        app_installs integer,
+                                        follows integer,
+                                        email_tweet text,
+                                        dial_phone text,
+                                        media_views integer,
+                                        media_engagements integer,
+                                        promoted_impressions integer,
+                                        promoted_engagements integer,
+                                        promoted_engagement_rate numeric,
+                                        promoted_retweets integer,
+                                        promoted_replies integer,
+                                        promoted_likes integer,
+                                        promoted_user_profile_clicks integer,
+                                        promoted_url_clicks integer,
+                                        promoted_hashtag_clicks integer,
+                                        promoted_detail_expands integer,
+                                        promoted_permalink_clicks integer,
+                                        promoted_app_opens integer,
+                                        promoted_app_installs integer,
+                                        promoted_follows integer,
+                                        promoted_email_tweet text,
+                                        promoted_dial_phone text,
+                                        promoted_media_views integer,
+                                        promoted_media_engagements integer
+                                        );
+                                        ''')
+
+    def writerow(self, row, conn):
+        # Insert a row of data
+        c = conn.cursor()
+        c.execute("INSERT INTO report VALUES "
+                  "(?,?,?,?,?,?,?,?,?,?,"
+                  "?,?,?,?,?,?,?,?,?,?,"
+                  "?,?,?,?,?,?,?,?,?,?,"
+                  "?,?,?,?,?,?,?,?,?,?)", (row))
+
+        # Save (commit) the changes
+        conn.commit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -171,6 +244,7 @@ if __name__ == '__main__':
     parser.add_argument('-d', help="Number of previous days' data to return (max: 90)", type=int, default=60)
     parser.add_argument('-o', help="Output directory", default=os.getcwd())
     parser.add_argument('-a', help="Account to return data for (default: -u)", required=False)
+    parser.add_argument('-t', help="Output type, options: csv, sqlite (default: -t csv)", required=False)
     args = parser.parse_args()
 
     USERNAME = args.u    
@@ -181,7 +255,7 @@ if __name__ == '__main__':
         ANALYTICS_ACCOUNT = USERNAME
     NUM_DAYS = args.d
     OUTPUT_DIRECTORY = args.o
-    
-    twitter_flow(USERNAME, PASSWORD, ANALYTICS_ACCOUNT, NUM_DAYS, OUTPUT_DIRECTORY)
+    OUTPUT_TYPE = args.t
+    twitter_flow(USERNAME, PASSWORD, ANALYTICS_ACCOUNT, NUM_DAYS, OUTPUT_DIRECTORY, OUTPUT_TYPE)
 
 
